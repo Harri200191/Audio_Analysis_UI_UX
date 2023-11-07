80ef65b91fa9e98289cb2494e4895fe5dcad4db1
haris rehman <harisrehmanchugtai@gmail.com>
2023-11-07 15:19:52 +0500

    Added the spacy package

--- final.py
+++ final.py
@@ -5,6 +5,55 @@ import shutil
 import moviepy.editor as mp 
 from pydub import AudioSegment 
 import spacy
+import os 
+
+#----------------------------------------------------------------------
+def split_audio(input_audio, output_dir, segment_length_ms):
+    print("Proccessing Input........")
+    # Load the audio file
+    audio = AudioSegment.from_file(input_audio)
+    
+    # Calculate the segment length in milliseconds
+    total_length_ms = len(audio)
+    
+    # Calculate the number of segments
+    num_segments = total_length_ms // segment_length_ms
+    
+    # Split the audio into 1-minute segments
+    for i in range(num_segments):
+        start_time = i * segment_length_ms
+        end_time = (i + 1) * segment_length_ms
+        segment = audio[start_time:end_time]
+        
+        # Save each segment as a separate audio file
+        segment.export(os.path.join(output_dir, f"segment_{i}.wav"), format="wav")
+    
+    # Split the last segment (if it's not a full minute)
+    if total_length_ms % segment_length_ms > 0:
+        start_time = num_segments * segment_length_ms
+        end_time = total_length_ms
+        last_segment = audio[start_time:end_time]
+        last_segment.export(os.path.join(output_dir, f"segment_{num_segments}.wav"), format="wav")
+
+def transcribe_audio_segments(segment_dir):
+    print("Converting to text........")
+    recognizer = sr.Recognizer()
+    
+    transcribed_texts = []
+    
+    for filename in os.listdir(segment_dir):
+        if filename.endswith(".wav"):
+            audio_file = os.path.join(segment_dir, filename)
+            
+            with sr.AudioFile(audio_file) as source:
+                audio_text = recognizer.record(source)
+                text = recognizer.recognize_google(audio_text)
+                transcribed_texts.append(text)
+    
+    return transcribed_texts
+
+def join_transcribed_texts(texts):
+    return " ".join(texts)
 
 # Load a spaCy NLP model
 nlp = spacy.load("en_core_web_sm")
@@ -69,35 +118,41 @@ def analyze_text(text):
  
 if __name__ == "__main__":
     mp3_file = select_and_store_file()  
-
-    if (mp3_file.split(".")[-1] == "mp3"):
-        result = mp3_to_text(mp3_file)
-
-        if "Error" in result:
-            print(result)
-        else:
-            print("Text from MP3 file:")
-            print(result)
-
-            person_count, topics = analyze_text(result)
+    output_directory = "audio_segments" 
     
-            print(f"Number of people speaking: {person_count}")
-            print("Topics or keywords:", topics)
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
+    
+    segment_length_ms = 60000  # 1 minute in milliseconds
+    
+    if (mp3_file.split(".")[-1] == "mp3"): 
+        split_audio(mp3_file, output_directory, segment_length_ms)
+        transcribed_texts = transcribe_audio_segments(output_directory)
+        final_transcribed_text = join_transcribed_texts(transcribed_texts)
+                    
+        print("\nFinal Transcribed Text:")
+        print(final_transcribed_text)
+
+        person_count, topics = analyze_text(final_transcribed_text)
+
+        print(f"Number of people speaking: {person_count}")
+        print("Topics or keywords:", topics)
     
     else:
         converted_tomp3 = video_to_mp3(mp3_file)
-        result = mp3_to_text(converted_tomp3)
+        split_audio(converted_tomp3, output_directory, segment_length_ms)
+        transcribed_texts = transcribe_audio_segments(output_directory)
+        final_transcribed_text = join_transcribed_texts(transcribed_texts)
+                    
+        print("\nFinal Transcribed Text:")
+        print(final_transcribed_text)
 
-        if "Error" in result:
-            print(result)
-        else:
-            print("Text from MP3 file:")
-            print(result)
+        person_count, topics = analyze_text(final_transcribed_text)
 
-            person_count, topics = analyze_text(result)
+        print(f"Number of people speaking: {person_count}")
+        print("Topics or keywords:", topics)
     
-            print(f"Number of people speaking: {person_count}")
-            print("Topics or keywords:", topics)
 
  
         
